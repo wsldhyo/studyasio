267c61aa48544ea68095166566bae0589b80747e
wsldhyo <wsldhyo@gmail.com>
2025-02-13 21:42:11 +0800

    1.服务器退出时,停止io_context服务, 结束io_context.run的阻塞 ,从而让整个线程退出 2.LogSystem析构时也等待工作线程执行完毕,从而保证逻辑队列数据都被处理

--- S08_MsgID_CS/server/src/main.cc
+++ S08_MsgID_CS/server/src/main.cc
@@ -1,17 +1,74 @@
-#include "../include/server.hpp"
+
+#include <asio/io_context.hpp>
+#include <asio/signal_set.hpp>
+#include <condition_variable>
+#include <csignal>
 #include <exception>
 #include <iostream>
+#include <mutex>
+#include <thread>
 
-int main(int argc, char* argv[])
+#include "../include/server.hpp"
+
+bool g_stop_server{false};
+std::mutex g_quit_mutex;
+std::condition_variable g_quit_cond;
+
+void signal_handler(int sig) {
+  if (sig == SIGINT || sig == SIGTERM) {
+    std::unique_lock<std::mutex> ulock(g_quit_mutex);
+    g_stop_server = true;
+    g_quit_cond.notify_one();
+  }
+}
+
+
+void cstyle_quit_server()
 {
-    try{
-        asio::io_context ioc;
-        //ServerErr s(ioc, 10086);
-        CServer s(ioc, 10086);
-        ioc.run(); // 启动事件循环
+
+  try {
+    asio::io_context ioc;
+    // ServerErr s(ioc, 10086);
+
+    std::thread net_work_thread([&ioc]() {
+      CServer s(ioc, 10086);
+      ioc.run(); // 启动事件循环
+    });
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+    
+    while(!g_stop_server)
+    {
+        std::unique_lock<std::mutex> ulock(g_quit_mutex);
+        g_quit_cond.wait(ulock);
     }
-    catch(std::exception& e){
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-    return 0;
+
+    ioc.stop(); // 关闭ioc服务
+    net_work_thread.join();
+  } catch (std::exception &e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+  }
+}
+
+
+
+
+void asio_quit_server(){
+    try {
+    asio::io_context ioc;
+    // 注册信号事件
+    asio::signal_set signal(ioc, SIGINT, SIGTERM);
+    signal.async_wait([&ioc](){
+        // signal注册的信号触发时，停止ioc服务，
+        ioc.stop();
+    });
+  } catch (std::exception &e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+  }
+    
+}
+
+int main(int argc, char *argv[]) {
+    asio_quit_server();
+  return 0;
 }
